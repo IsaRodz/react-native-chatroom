200e47889d8dbd32658c4c149eef49de992d92b8
IsaRodz <rodrigueziriarte96@gmail.com>
2020-10-17 23:08:58 -0500

    made google signin

--- components/SignIn.js
+++ components/SignIn.js
@@ -1,11 +1,79 @@
 import React from 'react';
 import { View, Button, Text, StyleSheet } from 'react-native';
+import * as Google from 'expo-google-app-auth';
+import googleClientId from '../config/google';
 import firebase, { auth } from '../firebase';
 
 const SignIn = () => {
-  const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithRedirect(provider);
+  function isUserEqual(googleUser, firebaseUser) {
+    if (firebaseUser) {
+      const { providerData } = firebaseUser;
+      for (let i = 0; i < providerData.length; i++) {
+        if (
+          providerData[i].providerId ===
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+          providerData[i].uid === googleUser.getBasicProfile().getId()
+        ) {
+          // We don't need to reauth the Firebase connection.
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  function onSignIn(googleUser) {
+    console.log('Google Auth Response', googleUser);
+    // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+    const unsubscribe = firebase.auth().onAuthStateChanged(firebaseUser => {
+      unsubscribe();
+      // Check if we are already signed-in Firebase with the correct user.
+      if (!isUserEqual(googleUser, firebaseUser)) {
+        // Build Firebase credential with the Google ID token.
+        let credential = firebase.auth.GoogleAuthProvider.credential(
+          googleUser.idToken,
+          googleUser.accessToken
+        );
+        // Sign in with credential from the Google user.
+        firebase
+          .auth()
+          .signInWithCredential(credential)
+          .then(() => console.log('user signed in'))
+          .catch(error => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            const credential = error.credential;
+            // ...
+          });
+      } else {
+        console.log('User already signed-in Firebase.');
+      }
+    });
+  }
+
+  const signInWithGoogle = async () => {
+    console.log('sign in with google 1');
+    try {
+      const result = await Google.logInAsync({
+        behavior: 'web',
+        androidClientId: googleClientId,
+        scopes: ['profile', 'email'],
+      });
+
+      if (result.type === 'success') {
+        onSignIn(result);
+        return result.accessToken;
+      } else {
+        return { cancelled: true };
+      }
+    } catch (e) {
+      console.log({ error: e });
+      return { error: e };
+    }
   };
 
   return (
---
+++ config/google.js
@@ -0,0 +1,1 @@
+export default '660040631410-sqg76t3u6f7409fltihmk6vma29j287c.apps.googleusercontent.com';
